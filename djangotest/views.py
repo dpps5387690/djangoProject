501d5fef67c70b6d9ac7ae1450032b9b7aaf88b0
dpps5387690 <dpps5387690@gmail.com>
2022-09-19 10:22:01 +0800

    log show

--- djangotest/views.py
+++ djangotest/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render
 
 mysqlhost = "gigabytenandteam.ddns.net"
 mysqlport = 33307
+
+# mysqlhost = "192.168.50.5"
+# mysqlport = 3307
+
+# mysqlhost = "10.1.8.91"  # "gigabytenandteam.ddns.net"
+# mysqlport = 3306  # 33307
+
 mysqluser = "hywu"
 mysqlpw = "kOsJX0GfsqIzeukj"
 

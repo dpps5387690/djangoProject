929e31d7b8114e347c334804a449e78d8be02140
dpps5387690 <dpps5387690@gmail.com>
2023-02-03 11:34:50 +0800

    remove build.sh change view to gigabytenandteam.ddns.net

--- djangotest/views.py
+++ djangotest/views.py
@@ -3,14 +3,14 @@ import pymysql.cursors
 from django.http import JsonResponse
 from django.shortcuts import render
 
-# mysqlhost = "gigabytenandteam.ddns.net"
-# mysqlport = 33307
+mysqlhost = "gigabytenandteam.ddns.net"
+mysqlport = 33307
 
 # mysqlhost = "192.168.50.5"
 # mysqlport = 3307
 
-mysqlhost = "10.1.8.91"  # "gigabytenandteam.ddns.net"
-mysqlport = 3306  # 33307
+# mysqlhost = "10.1.8.91"  # "gigabytenandteam.ddns.net"
+# mysqlport = 3306  # 33307
 
 mysqluser = "hywu"
 mysqlpw = "kOsJX0GfsqIzeukj"
